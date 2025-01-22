95b0b51e4cd16e9d0adbac1bbcf642db862ea8c4
sirojiddin2529 <suyarovsirojiddin143@gmail.com>
2025-01-22 13:21:50 +0500

    bajarildi

--- main.py
+++ main.py
@@ -1,87 +1,42 @@
+import json
+from pprint import pprint
+from services.authentications import register, login
+
 from models.user import User
-from models.room import Room
 from models.book import Book
-from services import authentications, booking, filters
+from models.room import Room
+from menu import menu, book_user
 
-
-def show_menu(menu: list[tuple[int, str]]):
-    for option in menu:
-        print(f"{option[0]} - {option[1]}")
 
 def main():
-    choice = 0
-
-    user: User = None
-    rooms: list[Room] = []
-    users: list[User] = []
-    session: list[User] = []
+    users: list[User]    = [] 
+    rooms: list[Room]    = []
+    session: list[User]  = []
     bookings: list[Book] = []
-
-    guest_menu = [
-        (1, "Register"),
-        (2, "Login"),
-        (3, "Exit")
-    ]
-
-    user_menu = [
-        (1, "See available rooms"),
-        (2, "Book a room"),
-        (3, "Filter rooms"),
-        (4, "Logout"),
-        (5, "Exit")
-    ]
+    
+    with open("files/rooms.json", "r", encoding="utf-8") as fayl:
+        rooms = json.load(fayl) 
 
     while True:
-
-        a_rooms = booking.available_rooms(rooms, bookings)
-
-        while user:
-            show_menu(user_menu)
-            choice = input("choice: ")
-
-            if choice == "1":
-                for a_room in a_rooms:
-                    print(f"Room number: {a_room.number}, Size: {a_room.size}, Price: {a_room.price}, Type: {a_room.type}")
-
-            elif choice == "2":
-                book = booking.booking(user, a_rooms)
-                bookings.append(book)
-                a_rooms.remove(book.room)
-
-            elif choice == "3":
-                size = int(input("size: "))
-                price = float(input("price: "))
-
-                filtered_rooms = filters.filter_rooms(a_rooms, size, price)
-                for room in filtered_rooms:
-                    print(f"Room number: {room.number}, Size: {room.size}, Price: {room.price}, Type: {room.type}")
-
-            elif choice == "4":
-                session = authentications.logout(user, session)
-                user = None
-            elif choice == "5":
-                exit()
+        menu()
+        choose = input("Buyruqni kiriting: ")
+        if choose == '1':
+            
+            users_new = register(users)
+            bookings.append(book_user(rooms, users_new))
+            users.append(users_new)
+        elif choose == '2':
+            user_booking = login(users)
+            if user_booking:
+                print("Mexmonnxonaga hush kelibsi! :)")
+                
+                bookings.append(book_user(rooms, user_booking))
             else:
-                print("invalid option")
+                print("Login yoki parol xato!\n")
+        elif choose == '3':
+            print("Buncha tez ketyabsiz?\n")
+            exit()
+        else:
+            print("Bunday buyrug` mavjud emas, iltimos tekshirib qaytadan kiriting!\n")
 
-        while user == None:
-            show_menu(guest_menu)
-            choice = input("choice: ")
-
-            if choice == "1":
-                user = authentications.register(users)
-                users.append(user)
-                session.append(user)
-
-            elif choice == "2":
-                user = authentications.login(users)
-                if user:
-                    session.append(user)
-
-            elif choice == "3":
-                exit()
-            else:
-                print("invalid option")
-
-if __name__ == "__main__":
-    main()
+main()
