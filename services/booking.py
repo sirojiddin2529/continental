af65eda0b3fd44a8ed87ab22fca89f5fc52b44ab
sirojiddin2529 <suyarovsirojiddin143@gmail.com>
2025-01-22 13:21:01 +0500

    done

--- services/booking.py
+++ services/booking.py
@@ -2,11 +2,5 @@ from models.user import User
 from models.room import Room
 from models.book import Book
 
-def booking(user: User, rooms: list[Room]) -> Book:
-    room_number = int(input("room number: "))
-    for room in rooms:
-        if room.number == room_number:
-            return Book(user, room)
-        
-def available_rooms(rooms: list[Room], bookings: list[Book]):
-    return [room for room in rooms if not room in bookings]
+def booking(user: User, room: Room) -> Book:
+    pass
